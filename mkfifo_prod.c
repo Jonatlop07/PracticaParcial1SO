f4fce457248f32b9506180784c5d7411483b4bd2
Jonathan <jonatlop@gmail.com>
2021-08-22 10:10:59 -0500

    Fifo funciona

--- mkfifo_prod.c
+++ mkfifo_prod.c
@@ -17,14 +17,14 @@ void handle_error (int code, char *msg) {
 int main(int argc, char *argv[]) {
   size_t mem_size = atoi(argv[1]) * sizeof(char);
   int fd;
+  char *buffer = (char *) malloc(mem_size);
+  memset(buffer, '0', mem_size);
   char *fifo_name = "./mkfifo";
   char confirmation_char;
   handle_error(mkfifo(fifo_name, 0666), "\n-->Error en mkfifo()");
   clock_t begin, end;
   //while (1) {
     handle_error(fd = open(fifo_name, O_WRONLY), "\n-->Error en open()");
-    char *buffer = (char *) malloc(mem_size);
-    memset(buffer, '0', mem_size);
     begin = clock();
     handle_error(write(fd, buffer, mem_size), "\n-->Error en write()");
     printf("\nBuff %s", buffer);
